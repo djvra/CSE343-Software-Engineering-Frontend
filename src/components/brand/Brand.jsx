40c608e0a6da060f215ccc094aa74d0b10924d1c
A. Elif Büyük <76538049+djvra@users.noreply.github.com>
2022-10-18 00:10:30 +0300

    Add files via upload

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -14,12 +14,48 @@ const Brand = () => {
   return (
     <div className='brand'>
 
-      <div class="filters">  
-        <form class="search-page-form" id="search-page-form" action="/search" method="GET">
-          <input type="submit" value="Search" class="search-page-form__submit" />
-          <hr class="solid" />
+      <div className="filters">  
+        <form className="search-page-form" id="search-page-form" action="/search" method="GET">
+          <input type="submit" value="Search" className="search-page-form__submit" />
 
-            <h3 class='filter-header'>Location</h3>
+          <hr className="solid" />
+          <h3 className='category-header'>CATEGORIES</h3>
+
+          <ul className="search-page-form__categories">
+            <li>
+                <input type="checkbox" id="category-0" name="category[]" class="js-ml-checkbox region-input" value="category-0"/>
+                <label for="category-0" class="category-label"> Category0</label>
+            </li>
+
+            <li>
+                <input type="checkbox" id="category-1" name="category[]" class="js-ml-checkbox region-input" value="category-1"/>
+                <label for="category-1" class="category-label"> Category1</label>
+            </li>
+
+            <li>
+                <input type="checkbox" id="category-2" name="category[]" class="js-ml-checkbox region-input" value="category-2"/>
+                <label for="category-2" class="category-label"> Category2</label>
+            </li>
+
+            <li>
+                <input type="checkbox" id="category-3" name="category[]" class="js-ml-checkbox region-input" value="category-3"/>
+                <label for="category-3" class="category-label"> Category3</label>
+            </li>
+          </ul>
+
+          <hr className="solid" />
+          <h3 className='category-header'>FOLLOWERS</h3>
+
+
+          <div class="numeric-input-holder clearfix">
+            <input type="text" class="numericInput js-manual-search-input" placeholder="min followers" name="followers_min" maxlength="11" data-section="followers" value=""/>
+          </div>
+          
+        
+
+          
+
+
         </form>
       </div>
       
